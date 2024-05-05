92a1247f2bc70f8fb5f19c2eddc03b8af1591617
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-06 10:49:03 +1200

    Update If Else.py

--- If Else.py
+++ If Else.py
@@ -2,3 +2,5 @@ print("You are exploring a rainforest in search of treasure.Legend has it that t
 action=input("You come across a lake.Do you want to swim across, or wait for a boat?")
 if action=="swim":
   print("You get eaten by a hungry shark. Game over.")
+else:
+  print("A boat arrives and you arrive at the island safely.")
