65540ea01c8b32a4105f2d2991e9c98f169c84aa
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-05 16:56:08 +1200

    Update If Else.py

--- If Else.py
+++ If Else.py
@@ -1,3 +1,3 @@
 print("You are exploring a rainforest in search of treasure.Legend has it that there are some buried on a nearby island.")
-
+input("You come across a lake.Do you want to swim across, or wait for a boat?")
 
